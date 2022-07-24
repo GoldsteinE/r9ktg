d79ecc03cc8d475d53350755edbdffdc425b2266
Goldstein <root@goldstein.rs>
2022-07-24 13:48:03 +0300

    allow/forbid, duplicates in replies

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::{borrow::Cow, fmt, path::PathBuf, sync::Arc};
+use std::{borrow::Cow, fmt, future::Future, path::PathBuf, sync::Arc};
 
 use color_eyre::eyre;
 use serde::Deserialize;
 use size_format::SizeFormatterBinary;
+use sled::CompareAndSwapError;
 use teloxide::{
     dispatching::UpdateFilterExt,
     dptree,
@@ -10,8 +11,8 @@ use teloxide::{
     payloads::SendMessageSetters as _,
     prelude::{Dispatcher, Request as _, Requester as _},
     types::{
-        ChatId, Document, MediaDocument, MediaKind, Message, MessageCommon, MessageKind, Update,
-        User,
+        Chat, ChatId, Document, MediaDocument, MediaKind, MediaText, Message, MessageCommon,
+        MessageKind, Update, User,
     },
     Bot,
 };
@@ -35,6 +36,8 @@ struct Config {
     db_path: PathBuf,
     #[serde(default = "default_max_import_size")]
     max_import_size: u32,
+    #[serde(default)]
+    allow_duplicates_in_replies: bool,
 }
 
 fn default_max_import_size() -> u32 {
@@ -97,112 +100,199 @@ struct Robot9000 {
 }
 
 impl Robot9000 {
-    fn store_message(&mut self, chat_id: ChatId, text: impl AsRef<[u8]>) -> sled::Result<bool> {
+    fn hash_message(&mut self, chat_id: ChatId, text: impl AsRef<[u8]>) -> [u8; 16] {
         self.hasher.reset();
         self.hasher.update(&chat_id.0.to_le_bytes());
         self.hasher.update(text.as_ref());
-        let hash = self.hasher.digest128().to_le_bytes();
-        self.db.insert(&hash, &[]).map(|x| x.is_some())
+        self.hasher.digest128().to_le_bytes()
+    }
+
+    fn store_message(&mut self, chat_id: ChatId, text: impl AsRef<[u8]>) -> eyre::Result<bool> {
+        let hash = self.hash_message(chat_id, text);
+        match self.db.compare_and_swap(&hash, None::<&[u8]>, Some(&[]))? {
+            Err(CompareAndSwapError {
+                current: Some(current),
+                ..
+            }) => Ok(current.len() == 0),
+            _ => Ok(false),
+        }
+    }
+
+    fn allow_message(&mut self, chat_id: ChatId, text: impl AsRef<[u8]>) -> eyre::Result<()> {
+        let hash = self.hash_message(chat_id, text);
+        self.db.insert(&hash, &[1])?;
+        Ok(())
+    }
+
+    fn forbid_message(&mut self, chat_id: ChatId, text: impl AsRef<[u8]>) -> eyre::Result<()> {
+        let hash = self.hash_message(chat_id, text);
+        self.db.insert(&hash, &[])?;
+        Ok(())
+    }
+
+    async fn is_admin(bot: &Bot, chat: &Chat, user: &User) -> eyre::Result<bool> {
+        Ok(chat.is_private()
+            || bot
+                .get_chat_member(chat.id, user.id)
+                .send()
+                .await?
+                .can_delete_messages())
+    }
+
+    async fn ensure_admin<Fut>(
+        bot: &Bot,
+        message: &Message,
+        user: &User,
+        f: Fut,
+    ) -> eyre::Result<()>
+    where
+        Fut: Future<Output = eyre::Result<()>>,
+    {
+        if !Self::is_admin(bot, &message.chat, user).await? {
+            tracing::info!(user_id = user.id.0, "someone tried to run admin command");
+            bot.send_message(message.chat.id, "Nice try")
+                .reply_to_message_id(message.id)
+                .send()
+                .await?;
+            Ok(())
+        } else {
+            f.await
+        }
     }
 
     async fn import_document(
         &mut self,
-        bot: Bot,
+        bot: &Bot,
         user: &User,
         message: &Message,
         document: &Document,
     ) -> eyre::Result<()> {
-        let import_allowed = message.chat.is_private()
-            || bot
-                .get_chat_member(message.chat.id, user.id)
+        if document.file_size > self.config.max_import_size {
+            tracing::info!(
+                user_id = user.id.0,
+                file_size = document.file_size,
+                max_import_size = self.config.max_import_size,
+                "/import failed due to file size",
+            );
+            let reply = format!(
+                "Come on, there's no way I'll import a {}B file (my limit is {}B)",
+                SizeFormatterBinary::new(document.file_size.into()),
+                SizeFormatterBinary::new(self.config.max_import_size.into()),
+            );
+            bot.send_message(message.chat.id, reply)
+                .reply_to_message_id(message.id)
                 .send()
-                .await?
-                .can_delete_messages();
+                .await?;
+            return Ok(());
+        }
 
-        if import_allowed {
-            if document.file_size > self.config.max_import_size {
+        let mut file = Vec::with_capacity(document.file_size as usize);
+        let file_info = bot.get_file(&document.file_id).send().await?;
+        bot.download_file(&file_info.file_path, &mut file).await?;
+        match serde_json::from_slice::<Import>(&*file) {
+            Ok(import) => {
+                let imported_count = import
+                    .messages
+                    .into_iter()
+                    .filter_map(|import_message| {
+                        (import_message.r#type == "message").then(|| {
+                            self.store_message(message.chat.id, &*import_message.text.moo())
+                                .map(|b| usize::from(!b))
+                        })
+                    })
+                    .sum::<Result<usize, _>>()?;
                 tracing::info!(
                     user_id = user.id.0,
-                    file_size = document.file_size,
-                    max_import_size = self.config.max_import_size,
-                    "/import failed due to file size",
+                    count = imported_count,
+                    "/import succeeded"
                 );
+
                 let reply = format!(
-                    "Come on, there's no way I'll import a {}B file (my limit is {}B)",
-                    SizeFormatterBinary::new(document.file_size.into()),
-                    SizeFormatterBinary::new(self.config.max_import_size.into()),
+                    "Sucessfully imported {imported_count} messages (excluding duplicates)"
                 );
                 bot.send_message(message.chat.id, reply)
                     .reply_to_message_id(message.id)
                     .send()
                     .await?;
-                return Ok(());
             }
-
-            let mut file = Vec::with_capacity(document.file_size as usize);
-            let file_info = bot.get_file(&document.file_id).send().await?;
-            bot.download_file(&file_info.file_path, &mut file).await?;
-            match serde_json::from_slice::<Import>(&*file) {
-                Ok(import) => {
-                    let imported_count = import
-                        .messages
-                        .into_iter()
-                        .filter_map(|import_message| {
-                            (import_message.r#type == "message").then(|| {
-                                self.store_message(message.chat.id, &*import_message.text.moo())
-                                    .map(|b| usize::from(!b))
-                            })
-                        })
-                        .sum::<Result<usize, _>>()?;
-                    tracing::info!(
-                        user_id = user.id.0,
-                        count = imported_count,
-                        "/import succeeded"
-                    );
-
-                    let reply = format!(
-                        "Sucessfully imported {imported_count} messages (excluding duplicates)"
-                    );
-                    bot.send_message(message.chat.id, reply)
-                        .reply_to_message_id(message.id)
-                        .send()
-                        .await?;
-                }
-                Err(err) => {
-                    tracing::info!(
-                        user_id = user.id.0,
-                        err = format_args!("{err}"),
-                        "/import failed due to deserialization error",
-                    );
-                    let reply = format!("Failed to parse your import, sorry :(\nError: {err}");
-                    bot.send_message(message.chat.id, reply)
-                        .reply_to_message_id(message.id)
-                        .send()
-                        .await?;
-                }
+            Err(err) => {
+                tracing::info!(
+                    user_id = user.id.0,
+                    err = format_args!("{err}"),
+                    "/import failed due to deserialization error",
+                );
+                let reply = format!("Failed to parse your import, sorry :(\nError: {err}");
+                bot.send_message(message.chat.id, reply)
+                    .reply_to_message_id(message.id)
+                    .send()
+                    .await?;
             }
-        } else {
-            tracing::info!(
-                user_id = user.id.0,
-                "someone tried to /import file without being an administrator"
-            );
-            bot.send_message(message.chat.id, "Only admins can /import")
-                .reply_to_message_id(message.id)
-                .send()
-                .await?;
         }
+
         Ok(())
+    }
+
+    async fn reply_command(
+        &mut self,
+        bot: &Bot,
+        message: &Message,
+        reply_to: &Message,
+        user: &User,
+        text: &str,
+    ) -> eyre::Result<bool> {
+        let reply_to_text = match &reply_to.kind {
+            MessageKind::Common(MessageCommon {
+                media_kind: MediaKind::Text(MediaText { text, .. }),
+                ..
+            }) => text,
+            _ => return Ok(false),
+        };
+
+        match text.trim() {
+            "/allow" => {
+                tracing::info!(allowed_message_id = reply_to.id, "allowed message");
+                Self::ensure_admin(bot, message, user, async {
+                    self.allow_message(message.chat.id, reply_to_text)
+                })
+                .await?;
+                Ok(true)
+            }
+            "/forbid" => {
+                tracing::info!(allowed_message_id = reply_to.id, "forbade message");
+                Self::ensure_admin(bot, message, user, async {
+                    self.forbid_message(message.chat.id, reply_to_text)
+                })
+                .await?;
+                Ok(true)
+            }
+            _ => Ok(false),
+        }
     }
 
     async fn process_message(&mut self, message: Message, bot: Bot) -> eyre::Result<()> {
         if let MessageKind::Common(
             kind @ MessageCommon {
-                from: Some(user), ..
+                from: Some(user),
+                reply_to_message,
+                ..
             },
         ) = &message.kind
         {
             match &kind.media_kind {
                 MediaKind::Text(text) => {
+                    if let Some(reply_to) = reply_to_message {
+                        if self
+                            .reply_command(&bot, &message, reply_to, user, &text.text)
+                            .await?
+                        {
+                            return Ok(());
+                        }
+
+                        if self.config.allow_duplicates_in_replies {
+                            return Ok(());
+                        }
+                    }
+
                     if self.store_message(message.chat.id, &text.text)? {
                         tracing::debug!(
                             text = format_args!("{:?}", text.text),
@@ -224,7 +314,13 @@ impl Robot9000 {
                     ..
                 }) => {
                     if caption.trim() == "/import" {
-                        self.import_document(bot, user, &message, document).await?;
+                        Self::ensure_admin(
+                            &bot,
+                            &message,
+                            user,
+                            self.import_document(&bot, user, &message, document),
+                        )
+                        .await?;
                     }
                 }
                 _ => (),
