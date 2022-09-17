7806b1e08b0bc2659331f46ac4963be07d36c940
Goldstein <root@goldstein.rs>
2022-09-17 13:44:40 +0300

    remove unneeded overlay

--- flake.nix
+++ flake.nix
@@ -1,29 +1,19 @@
 {
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
-    naersk.url       = "github:nix-community/naersk";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk }:
+  outputs = { self, nixpkgs, flake-utils, naersk }:
     flake-utils.lib.eachDefaultSystem (system:
-      let 
-        overlays = [ (import rust-overlay) ];
+      let
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
         };
-        rust = (pkgs.rust-bin.stable.latest.default.override {
-          extensions = [
-            "rust-src"
-            "cargo"
-            "rustc"
-            "rustfmt"
-          ];
-        });
         naersk-lib = naersk.lib."${system}".override {
-          cargo = rust;
-          rustc = rust;
+          cargo = pkgs.cargo;
+          rustc = pkgs.rustc;
         };
       in rec {
         packages.r9ktg = naersk-lib.buildPackage {
@@ -36,29 +26,27 @@
         defaultApp = apps.r9ktg;
 
         nixosModules.default = with pkgs.lib; { config, ... }:
-        let cfg = config.services.r9ktg;
-        in {
-          options.services.r9ktg = {
-            enable = mkEnableOption "Robot9000 for Telegram";
-            envFile = mkOption {
-              type = types.str;
-              default = "/etc/r9ktg.env";
+          let cfg = config.services.r9ktg;
+          in
+          {
+            options.services.r9ktg = {
+              enable = mkEnableOption "Robot9000 for Telegram";
+              envFile = mkOption {
+                type = types.str;
+                default = "/etc/r9ktg.env";
+              };
+            };
+            config = mkIf cfg.enable {
+              systemd.services.r9ktg = {
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig.ExecStart = "${self.defaultPackage.${system}}/bin/r9ktg";
+                serviceConfig.EnvironmentFile = cfg.envFile;
+              };
             };
           };
-          config = mkIf cfg.enable {
-            systemd.services.r9ktg = {
-              wantedBy = [ "multi-user.target" ];
-              serviceConfig.ExecStart = "${self.defaultPackage.${system}}/bin/r9ktg";
-              serviceConfig.EnvironmentFile = cfg.envFile;
-            };
-          };
-        };
 
         devShell = pkgs.mkShell {
-          buildInputs = [
-            rust
-            pkgs.rust-analyzer
-          ];
+          buildInputs = with pkgs; [ cargo rustc rustfmt clippy rust-analyzer ];
         };
       }
     );
